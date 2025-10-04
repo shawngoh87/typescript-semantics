88aac3f6fd63c7a6b1c469c7ae943c82eee8276d
Shawn Goh <shawngoh87@gmail.com>
2025-10-04 19:41:05 +0800

    feat: Add basic types.

---
+++ tests/basic/example-2.ts
@@ -0,0 +1,15 @@
+let type1: number;
+let type2: string;
+let type3: boolean;
+let type4: any;
+let type5: unknown;
+let type6: never;
+let type7: void;
+let type8: null;
+let type9: undefined;
+let type10: symbol;
+let type11: object;
+let type12: bigint;
+
+// @ts-expect-error
+number1 = true;
